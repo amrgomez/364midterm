01bf4cadf77785fd1766adfbbfaaaf6080291020
amrgomez <32075935+amrgomez@users.noreply.github.com>
2018-03-11 23:56:49 -0400

    Add files via upload

--- SI364midterm.py
+++ SI364midterm.py
@@ -7,11 +7,14 @@
 import os
 from flask import Flask, render_template, session, redirect, url_for, flash, request
 from flask_wtf import FlaskForm
-from wtforms import StringField,SubmitField,ValidationError, IntegerField # Note that you may need to import more here! Check out examples that do what you want to figure out what.
+from wtforms import StringField,SubmitField,ValidationError # Note that you may need to import more here! Check out examples that do what you want to figure out what.
 from wtforms.validators import Required, Length
 from flask_sqlalchemy import SQLAlchemy
 import omdb
+import requests
+import json
 
+#api= 'http://www.omdbapi.com/?i=tt3896198&apikey=12fe4e3b'
 
 ## App setup code
 app = Flask(__name__)
@@ -36,9 +39,17 @@ db = SQLAlchemy(app)
 ######################################
 ######## HELPER FXNS (If any) ########
 ######################################
-
-
-
+def get_or_create_movie(movies):
+    title= db.session.query(MovieTitle).filter_by(movies=movies).first()
+    if not title:
+        title= MovieTitle(movies= movies)
+        db.session.add(title)
+        db.session.commit()
+        flash("Movie successfully added!")
+        return title
+    else:
+        flash("Movie already added!")
+        return title
 
 ##################
 ##### MODELS #####
@@ -50,17 +61,17 @@ class MovieTitle(db.Model):
     __tablename__= 'movies'
     id= db.Column(db.Integer, primary_key=True)
     movies= db.Column(db.String(64))
-    years= db.relationship('Year', backref= 'MovieTitle')
+    years= db.relationship('Years', backref= 'MovieTitle')
     def __repr__(self):
-        return "{} (ID: {})".format(self.movies, self.id)
+        return "{}".format(self.movies)
 
-class Year(db.Model):
+class Years(db.Model):
     __tablename__= 'years'
     id= db.Column(db.Integer, primary_key= True)
-    year= db.Column(db.Integer)
-    year_id=db.Column(db.Integer,db.ForeignKey('movies.id'))
+    year= db.Column(db.String(64))
+    movie_id=db.Column(db.Integer,db.ForeignKey('movies.id'))
     def __repr__(self):
-        return "{} (ID: {})".format(self.year, self.id)
+        return "{}".format(self.year)
         
 class Name(db.Model):
     __tablename__ = "names"
@@ -76,15 +87,17 @@ class Name(db.Model):
 ###################
 class MovieForm(FlaskForm):
     movie_title= StringField('Enter a movie: ',validators= [Required(),Length(min=1,max=280)])
-    years= IntegerField('Enter the year the movie was released: ', validators=[Required(),Length(min=2,max=4)])
     submit= SubmitField('Submit')
 
     def validate_movie_title(self,field):
         if field.data[0] is '~':
             return ValidationError('Title Invalid')
+    def validate_release_year(self,field):
+        if field.data[0] is '0':
+            return ValidationError('Invalid Year')
 
 class NameForm(FlaskForm):
-    name = StringField("Please enter your name:",validators=[Required(),Length(min=1,max=280)])
+    name = StringField("Please enter your name: ",validators=[Required(),Length(min=1,max=280)])
     submit = SubmitField('Submit')
 
     def validate_name(self,field):
@@ -110,32 +123,38 @@ def internal_server_error(e):
 @app.route('/',methods=['GET'])
 def home():
     form = NameForm() # User should be able to enter name after name and each one will be saved, even if it's a duplicate! Sends data with GET
-    if form.validate_on_submit():
-        name = form.name.data
-        newname = Name(name)
-        db.session.add(newname)
-        db.session.commit()
-        return redirect(url_for('all_names'))
     return render_template('base.html',form=form)
 
 @app.route('/names')
 def all_names():
-    names = Name.query.all()
-    return render_template('name_example.html',names=names)  
+    if request.args:
+        name = request.args['name']
+        newname = Name(name=name)
+        db.session.add(newname)
+        db.session.commit()
+        return redirect(url_for('all_names'))
+    return render_template('name_example.html',names=Name.query.all())  
 
 @app.route('/movie_titles', methods= ['GET','POST'])
 def movies():
-    form1=MovieForm(request.form)
-    if form.validate_on_submit():
-        movies=form.movies.data
-    return render_template('movie_titles.html',form=form1)
+    form=MovieForm()
+    if request.method == "POST" and form.validate_on_submit():
+        movies1=form.movie_title.data
+        url= 'http://www.omdbapi.com/?apikey=12fe4e3b&t={}'.format(movies1)
+        req=requests.get(url)
+        txt= req.json()
+        all_movies=get_or_create_movie(txt['Title'])
+        add_year= Years(year=txt['Year'], movie_id= all_movies.id)
+        db.session.add(add_year)
+        db.session.commit()
+        redirect(url_for('years'))
+    return render_template('movie_titles.html',form=form)
 
-@app.route('/release_years', methods=['GET','POST'])
+@app.route('/release_years', methods=['GET', 'POST'])
 def years():
-    form1=MovieForm(request.form) 
-    if form.validate_on_submit():
-        years= form.years.data
-    return render_template('release_years.html',form=form1)
+    all_years= Years.query.all()
+    return render_template('years_released.html',all_years= all_years)
+
 
 
 ## Code to run the application...
